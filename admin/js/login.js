5e63b4edfbc13446e17d61940326a2a49bc3db75
Xeyyam456 <xeyyamelizade5@gmail.com>
2026-02-24 23:26:00 +0400

    feat: Enhance login page with password visibility toggle and new icon

--- admin/js/login.js
+++ admin/js/login.js
@@ -14,3 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+
+
+const passtoggle = document.querySelector("#pass-toggle");
+const passwordInput = document.querySelector("#password");
+
+passtoggle.addEventListener("click", () => {
+  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+});
+
